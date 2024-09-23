42b4ea7fade6bca8100ae22857659e8c94717f0a
Irasov <srnan@yandex.ru>
2024-09-23 12:28:03 +0500

    feat:release functions SetRange, guess, lower, greater

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.res = Math.ceil((this.max + this.min) / 2);
+        return this.res;
     }
 
     lower() {
-
+        this.max = this.res;
     }
 
     greater() {
-
+        this.min = this.res;
     }
 }
 
